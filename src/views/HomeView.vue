3d2880bc2a502ea9a7610c2eea20535fd1baa9b2
Jorge <jagedn@gmail.com>
2024-06-10 20:57:15 +0200

    Update HomeView.vue
    update author

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -236,6 +236,6 @@ if (!instance.getUser()) {
       </Card>
 
     </div>
-    <em>Made with love by @jorge@foojay.social </em>
+    <em>Made with love by @jorge@friendica.aguilera.soy </em>
   </div>
 </template>
